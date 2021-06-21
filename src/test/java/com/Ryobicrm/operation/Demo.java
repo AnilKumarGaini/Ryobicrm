edfce93973ffabffad7901773eb8902579ec9d40
DELL <DELL@DESKTOP-7BTJCGK>
2021-06-21 23:27:21 +0530

    First Commit

---
+++ src/test/java/com/Ryobicrm/operation/Demo.java
@@ -0,0 +1,16 @@
+package com.Ryobicrm.operation;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void run() {
+		
+		
+		Reporter.log("Demo are runnibg successfully", true);
+		
+	
+	}
+}
---
+++ src/test/java/com/Ryobicrm/operation/Remo.java
@@ -0,0 +1,16 @@
+package com.Ryobicrm.operation;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Remo {
+	
+	@Test
+	public void run() {
+		
+		
+		Reporter.log("Remo are runnibg successfully", true);
+		
+	
+	}
+}
